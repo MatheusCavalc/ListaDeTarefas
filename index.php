f23e0cb419959ebb2037a701f2c6427eea334262
Matheus Cavalcante <105112560+MatheusCavalc@users.noreply.github.com>
2022-08-15 12:45:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -116,9 +116,9 @@
 									<?= $tarefa->tarefa ?>
 								</div>
 								<div class="col-sm-3 mt-2 d-flex justify-content-between">
-									<i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?= $tarefa->id ?>)" ></i>
-									<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
-									<i class="fas fa-check-square fa-lg text-success" onclick="realizada(<?= $tarefa->id ?>)"></i>								
+									<i class="fas fa-trash-alt fa-lg text-danger" style="cursor: pointer;" onclick="remover(<?= $tarefa->id ?>)" ></i>
+									<i class="fas fa-edit fa-lg text-info" style="cursor: pointer;" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
+									<i class="fas fa-check-square fa-lg text-success" style="cursor: pointer;" onclick="realizada(<?= $tarefa->id ?>)"></i>								
 								</div>
 							</div>
 
